4e94b2a1ae2f64fe46cb0f13869dee3233c48510
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-27 15:07:58 +0900

    reseting initial display done

--- app.js
+++ app.js
@@ -1,14 +1,59 @@
 // View module
-const uiController = (function() {})();
+const uiController = (() => {
+  // DOM
+  const budgeValue = document.querySelector(".budget__value");
+  const incomeValue = document.querySelector(".budget__income--value");
+  const expenseValue = document.querySelector(".budget__expenses--value");
+  const thisMonth = document.querySelector(".budget__title--month");
+
+  return {
+    updateBadget: function(args) {
+      budgeValue.textContent = args.budget;
+      incomeValue.textContent = args.income;
+      expenseValue.textContent = args.expense;
+    },
+    displayMonth: function() {
+      const today = new Date();
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      thisMonth.textContent = `${
+        months[today.getMonth()]
+      },  ${today.getFullYear()}`;
+    }
+  };
+})();
 
 // Budget model
-const budgetController = (function() {})();
+const budgetController = (() => {
+  const data = {
+    income: 0,
+    expense: 0
+  };
+})();
 
 // Controller
-const controller = (function(uiController, budgetController) {
+const controller = ((uiController, budgetController) => {
   return {
     init: function() {
       console.log("Initialized");
+      uiController.updateBadget({
+        budget: 0,
+        income: 0,
+        expense: 0
+      });
+      uiController.displayMonth();
     }
   };
 })(uiController, budgetController);
